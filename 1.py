20a32a128462e281544844bc81a70d29b2726fda
Jose Baza <jose.baza@correo.usa.edu.co>
2019-11-19 12:30:05 -0500

    segundo punto

--- 1.py
+++ 1.py
@@ -8,14 +8,19 @@ from PIL import Image
 from PIL import ImageTk
 import time
 cont=0
-prom=0
+prom1=0
+prom2=0
+prom3=0
 
-placa = Arduino ('COM4')
+placa = Arduino ('COM6')
 it = util.Iterator(placa)
 it.start()
 a_0 = placa.get_pin('a:0:i')
 a_1 = placa.get_pin('a:1:i')
 a_2 = placa.get_pin('a:2:i')
+led = placa.get_pin('d:3:p')
+led1 = placa.get_pin('d:5:p')
+led2 = placa.get_pin('d:6:p')
 time.sleep(0.5)
 ventana = Tk()
 ventana.geometry('1090x545')
@@ -40,13 +45,14 @@ valor4= Label(marco1, bg='cadet blue1', font=("Arial Bold", 15), fg="white", wid
 adc_data3=StringVar()
 
 def adc_read():
-    global prom
+    global prom1
     i=0
     while i<15:
         i=i+1
         x=a_0.read()
         print(x)
         adc_data.set(x)
+        prom1=x+prom1
         ventana.update()
         time.sleep(0.1)
     ref = db.reference('sensor')
@@ -55,7 +61,7 @@ def adc_read():
     })
 
 def adc_read1():
-    global prom
+    global prom2
     i=0
     prom=0
     while i<15:
@@ -63,28 +69,36 @@ def adc_read1():
         y=a_1.read()
         print(y)
         adc_data2.set(y)
+        prom2=y+prom2
         ventana.update()
         time.sleep(0.1)
     ref = db.reference('sensor')
     ref.update({
-        'sensor1/adc': y
+        'sensor2/adc': y
     })
 
 def adc_read2():
-    global prom
+    global prom3
     i=0
     while i<15:
-        i=i+1
+        i=i+prom1
         z=a_2.read()
         print(z)
         adc_data3.set(z)
+        prom3=z+prom3
         ventana.update()
     ref = db.reference('sensor')
     ref.update({
-        'sensor1/adc': z
+        'sensor3/adc': z
     })
 
-  
+def update():
+    ref1=db.reference("sensor1/adc")
+    ref1=db.reference("sensor2/adc")
+    ref1=db.reference("sensor3/adc")
+    led.write(prom1)
+    led1.write(prom2)
+    led2.write(prom3)
 
 valor2.configure(textvariable=adc_data)
 valor2.place(x=130, y=160)
@@ -103,6 +117,9 @@ prom_15.place(x=10, y=200)
 
 prom_15=Button(marco1,text="adc3_update",command=adc_read2)
 prom_15.place(x=10, y=240)
+
+prom_15=Button(marco1,text="adc3_update",command=update)
+prom_15.place(x=10, y=280)
 
 
 
